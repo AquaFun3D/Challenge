1a1ae14a79b7b2f6e8f72fdd6fd80700e89ce15b
AquaFun3D <nino@theska.de>
2021-09-10 16:05:19 +0200

    Initial convert to Kotlin project

---
+++ src/main/java/com/aquafun3d/challenge/Main.kt
@@ -0,0 +1,13 @@
+package com.aquafun3d.challenge
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
